f9acf7638e04509f3356a9f2645d0942c099ab3c
Paulo_Braga <paulo@someletras.com.br>
2018-05-06 21:04:12 -0300

    New class config, load_text window

--- config.py
+++ config.py
@@ -5,27 +5,3 @@ class Config:
 		self.font_size = "25"
 		self.font_body = "bold"
 		self.x = x
-
-
-# 	def show(self):
-# 		print(self.font, self.font_size, self.font_body)
-
-# 	def showfont(self):
-# 		print(self.font)
-
-
-# conf = Config(10)
-# conf.show()
-
-
-# class Config2(Config):
-# 	def __init__(self, x, y):
-# 		super().__init__(x)
-# 		self.y = y
-
-# 	def show(self):
-# 		print(self.font, self.font_size, self.font_body, self.x, self.y)
-
-# conf2 = Config2(1, 10)
-# conf2.show()
-# conf2.showfont()
